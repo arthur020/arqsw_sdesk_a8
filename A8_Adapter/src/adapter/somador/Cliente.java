88cf17bcb62d9bb05372967ba61e5b55d03bb5cb
Arthur <arthurfelipe__@hotmail.com>
2018-05-15 16:50:08 -0300

    Aula8 - Commit inicial

---
+++ A8_Adapter/src/adapter/somador/Cliente.java
@@ -0,0 +1,18 @@
+package adapter.somador;
+
+public class Cliente {
+	@SuppressWarnings("unused")
+	private SomadorEsperado somador;
+
+	public Cliente(SomadorEsperado somador) {
+		this.somador = somador;
+	}
+
+	public void executar() {
+		SomadorExistente somadorExistente = new SomadorExistente();
+		int[] vetor = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int soma = somadorExistente.somaVetor(vetor);
+		System.out.println("Resultado: " + soma);
+	}
+}
+
---
+++ A8_Adapter/src/templateMethod/string/StringTransform.java
@@ -0,0 +1,16 @@
+package templateMethod.string;
+
+import java.util.Scanner;
+
+public abstract class StringTransform {
+	
+	@SuppressWarnings("resource")
+	protected void read() {
+		Scanner scan = new Scanner(System.in);
+		String text = scan.next();
+		text = transform(text);
+		System.out.println("Readed: " + text);
+	}
+	
+	public abstract String transform(String text);
+}
